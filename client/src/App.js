0ebc1b9c7f3e6499a9568d65094875b3d9d2321e
Kirsi Ralko <72715839+kirsralk@users.noreply.github.com>
2021-04-24 11:25:09 -0500

    add router | import navbar to app

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,24 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import logo from "./logo.svg";
 import "./App.css";
+import Nav from "./components/Navbar";
 
 
 function App() {
   return (
-    <div className="App">
-      <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h2>Welcome to React</h2>
+    <Router>
+      <div className="App">
+        <Nav />
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
       </div>
-      <p className="App-intro">
-        To get started, edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
+    </Router>
   );
 }
 
