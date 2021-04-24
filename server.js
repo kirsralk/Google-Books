b7f863f1e5731f46343a5e5dcf1c2fe8c161b3ae
Kirsi Ralko <72715839+kirsralk@users.noreply.github.com>
2021-04-24 10:36:36 -0500

    add MongoDB connection details

--- server.js
+++ server.js
@@ -19,6 +19,17 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
+// Connect to MongoDB
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/googlebooks',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
+
 app.listen(PORT, () => {
   console.log(`🌎 ==> API server now on port ${PORT}!`);
 });
